30169a26c091b120d2a25b36d7839e286a4d225c
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-02-21 16:08:45 +0700

    finish checkout page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import LandingPage from "pages/LandingPage"
 import DetailsPage from "pages/DetailsPage"
+import CheckoutPage from "pages/CheckoutPage"
 import InputNumber from "pages/ExampleInputNumber"
 import InputDate from "pages/ExampleInputDate"
 import Breadcrumb from "pages/ExampleBreadcrumb"
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={CheckoutPage} />
         <Route path="/exampleinputnumber" component={InputNumber} />
         <Route path="/exampleinputdate" component={InputDate} />
         <Route path="/examplebreadcrumb" component={Breadcrumb} />
